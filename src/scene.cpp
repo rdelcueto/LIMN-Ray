d06bfcfea03331b665e7f6c5da6b98b8451b7475
rdelcueto <rdelcueto@53a3f782-18c8-11de-82b5-194b17410922>
2009-04-01 18:06:37 +0000

    Changes:  - Shadows: Apparently fixed! :D
    git-svn-id: http://limn-ray.googlecode.com/svn/trunk@5 53a3f782-18c8-11de-82b5-194b17410922

--- src/scene.cpp
+++ src/scene.cpp
@@ -35,19 +35,19 @@ Scene::Scene() {
   plane_w = 640;
   plane_h = 640;
   planePos[0] = planePos[1] = planePos[2] = 0.0;
-  focalPointPos[0] = focalPointPos[1] = 0.0; focalPointPos[2] = -2.0;
+  focalPointPos[0] = focalPointPos[1] = 0.0; focalPointPos[2] = -2.8;
   lookAt[0] = lookAt[1] = 0.0; lookAt[2] = 1.0;
   recurLimit = 8;
   samples = 1;
 
-  sceneMaterials.push_back(new Material(1, 0.9, 0.9, 1, 0, 1, 1, 1, 1, 0.5));
-  sceneMaterials.push_back(new Material(0.9, 1, 0.9, 1, 0, 1, 1, 1, 1, 0.5));
-  sceneMaterials.push_back(new Material(0.9, 0.9, 1, 1, 0, 1, 1, 1, 1, 0.5));
+  sceneMaterials.push_back(new Material(1, 0.9, 0.9,  1.0, 0, 1, 1, 1, 0.0, 0.0));
+  sceneMaterials.push_back(new Material(0.9, 1, 0.9,  1.0, 0, 1, 1, 2, 1.0, 0.0));
+  sceneMaterials.push_back(new Material(0.9, 0.9, 1,  0.2, 0, 1, 1, 4, 0.25, 0.1));
   MaterialList::iterator mi = sceneMaterials.begin();
 
-  sceneLights.push_back(new Omnidirectional(0, 10, 100, 1, 0, 0, 2, 1));
-  sceneLights.push_back(new Omnidirectional(-14.14, -14.14, 100, 0, 1, 0, 2, 1));
-  sceneLights.push_back(new Omnidirectional(14.14, -14.14, 100, 0, 0, 1, 2, 1));
+  sceneLights.push_back(new Omnidirectional(0, 10, 80, 1, 0, 0, 3, 1));
+  sceneLights.push_back(new Omnidirectional(-14.14, -14.14, 80, 0, 1, 0, 3, 1));
+  sceneLights.push_back(new Omnidirectional(14.14, -14.14, 80, 0, 0, 1, 3, 1));
 
   sceneObjects.push_back(new Sphere(0, -5, 120, 5, *mi));
   mi++;
@@ -59,6 +59,7 @@ Scene::Scene() {
   sceneObjects.push_back(new Plane(20, 0, 0, -1, 0, 0));
   sceneObjects.push_back(new Plane(0, 20, 0, 0, -1, 0));
   sceneObjects.push_back(new Plane(0, -20, 0, 0, 1, 0));
+
 }
 
 void Scene::intersect(Ray *r) {
@@ -114,16 +115,16 @@ int Scene::shader(Ray *r) {
 
     blas3dSubstractXY(l->pos, intPoint, lVec); // Light Pos - Intersection
     lDistance = sqrt(blas3dDot(lVec, lVec));
+    blas3DNormalize(lVec);
 
     Ray *shadowRay = new Ray();
     shadowRay->setPos(intPoint);
-    shadowRay->setLookAt(lVec);
+    shadowRay->setDir(lVec);
     shadowRay->move(INTERSECT_EPSILON);
     intersect(shadowRay);
 
     if(shadowRay->t_intersect > lDistance ){
       lDistance += pDistance;
-      blas3DNormalize(lVec);
 
       lightI = l->intensity*LIGHT_SCALE/(lDistance*lDistance*l->damping);
       // Diffuse
