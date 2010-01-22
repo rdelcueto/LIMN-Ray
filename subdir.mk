a6127710fa2f96633fbdc51ede3dc9b5a2c9efd4
rdelcueto <rdelcueto@53a3f782-18c8-11de-82b5-194b17410922>
2010-01-22 00:54:22 +0000

    Fixed Make paraphernalia.
    git-svn-id: http://limn-ray.googlecode.com/svn/trunk@35 53a3f782-18c8-11de-82b5-194b17410922

---
+++ subdir.mk
@@ -0,0 +1,45 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+./src/blas.cpp \
+./src/lights.cpp \
+./src/limn-ray.cpp \
+./src/materials.cpp \
+./src/parser.cpp \
+./src/primitives.cpp \
+./src/rays.cpp \
+./src/scene.cpp 
+
+OBJS += \
+./src/blas.o \
+./src/lights.o \
+./src/limn-ray.o \
+./src/materials.o \
+./src/parser.o \
+./src/primitives.o \
+./src/rays.o \
+./src/scene.o 
+
+CPP_DEPS += \
+./src/blas.d \
+./src/lights.d \
+./src/limn-ray.d \
+./src/materials.d \
+./src/parser.d \
+./src/primitives.d \
+./src/rays.d \
+./src/scene.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ./src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O3 -fsingle-precision-constant -Wall -c -fmessage-length=0 `freetype-config --cflags` -fopenmp -ffast-math -mfpmath=sse -msse3 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
